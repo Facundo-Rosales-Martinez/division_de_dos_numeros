489febba1c089392dd7d9539cb44268a69a0e26f
Facundo <frm.code@gmail.com>
2024-04-27 11:31:49 -0300

    preparando para fusionar local y remoto

---
+++ division_de_dos_numeros.py
@@ -0,0 +1,8 @@
+dividendo = int(
+    input("Hola, haremos una division, introduce el dividendo : "))
+
+divisor = int(input("Introduce el segundo divisor : "))
+
+resultado = dividendo / divisor
+
+print("El resultado es ", dividendo, " / ", divisor, " = ", resultado)
